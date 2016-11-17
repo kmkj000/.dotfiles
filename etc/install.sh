98b04e7df6121fa9d667017363ef753b0e91b038
kmkj000 <kd1191681@st.kobedenshi.ac.jp>
2016-11-17 06:25:26 +0000

    fix install.sh

--- etc/install.sh
+++ etc/install.sh
@@ -4,24 +4,26 @@ DOTPATH=~/.dotfiles
 GITHUB_URL=git@github.com:kmkj000/.dotfiles.git
 
 # git が使えるなら git
-if has "git"; then
+if type "git" > /dev/null 2>&1; then
     git clone --recursive "$GITHUB_URL" "$DOTPATH"
 
 # 使えない場合は curl か wget を使用する
-elif has "curl" || has "wget"; then
+elif type"curl" > /dev/null 2>&1 || type "wget" > /dev/null 2>&1; then
     tarball="https://github.com/kmkj000/.dotfiles/archive/master.tar.gz"
 
     # どっちかでダウンロードして，tar に流す
-    if has "curl"; then
+    if type "curl" > /dev/null 2>&1; then
         curl -L "$tarball"
 
-    elif has "wget"; then
+    elif type "wget" > /dev/null 2>&1; then
         wget -O - "$tarball"
 
-    fi | tar xv -
+    fi | tar xvz 
 
     # 解凍したら，DOTPATH に置く
     mv -f dotfiles-master "$DOTPATH"
+
+    echo "Download Complete"
 
 else
     echo "curl or wget required"
