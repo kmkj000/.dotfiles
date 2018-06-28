5f111656db01747220cf8168a1d715c5fc8c2806
kmkj000 <kd1191681@st.kobedenshi.ac.jp>
2018-06-28 18:05:53 +0900

    🔨 separate install.sh

---
+++ setup/deploy.sh
@@ -0,0 +1,22 @@
+cd ~/.dotfiles
+if [ $? -ne 0 ]; then
+    echo  "not found: $DOTPATH"
+fi
+
+# 移動できたらリンクを実行する
+## makeが使える場合
+#if type "make" > /dev/null 2>&1; then
+#make
+#fi
+
+#シンボリックリンクをはる
+cd ~/
+find ~/.dotfiles -maxdepth 1 -name ".*" -not -name ".git" -not -name ".dotfiles" | xargs -n 1 ln -sf
+
+#DOT_FILES=(.bash_profile .vimrc .vim .local .tmux.conf)
+#for file in ${DOT_FILES[@]}
+#do
+#     ln -sf $DOTPATH/$file $HOME/$file
+#done
+
+echo "complete deploy!"
--- setup/install.sh
+++ setup/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bssh
+#!/bin/bash
 
 DOTPATH=~/.dotfiles
 GITHUB_URL=https://github.com/kmkj000/.dotfiles.git
@@ -30,23 +30,5 @@ else
     exit 1
 fi
 
-cd ~/.dotfiles
-if [ $? -ne 0 ]; then
-    echo  "not found: $DOTPATH"
-fi
-
-# 移動できたらリンクを実行する
-## makeが使える場合
-#if type "make" > /dev/null 2>&1; then
-#make
-#fi
-
-#シンボリックリンクをはる
-cd ~/
-find ~/.dotfiles -maxdepth 1 -name ".*" -not -name ".git" -not -name ".dotfiles" | xargs -n 1 ln -sf
-
-#DOT_FILES=(.bash_profile .vimrc .vim .local .tmux.conf)
-#for file in ${DOT_FILES[@]}
-#do
-#     ln -sf $DOTPATH/$file $HOME/$file
-#done
+# 取得できたらデプロイ
+~/.dotfiles/setup/deploy.sh
